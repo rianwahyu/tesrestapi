1aff53a1a24c71c13cb7e02ab273c1f27f678ea9
rian <rianwahyu26@gmail.com>
2020-10-16 17:07:11 +0700

    menampilkan data mahasiswa by ID

--- controller.js
+++ controller.js
@@ -13,9 +13,23 @@ exports.index = function(req, res){
 exports.tampilDataMahasiswa = function(req, res){
     connection.query('SELECT * FROM MAHASISWA', function(error, rows, fileds) {
         if(error){
-            connection.log(error);
+            console.log(error);
         }else {
             response.ok(rows, res)
         }
+    });
+}
+
+//menampilkan semua data mahasiswa beradarkan id 
+
+exports.tampilMahasiswabyID = function(req, res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id = ?', [id], 
+        function(error, rows, fields){
+            if(error){
+                console.log(error);
+            }else{
+                response.ok(rows, res);
+            }
     });
 }
--- routes.js
+++ routes.js
@@ -8,4 +8,7 @@ module.exports = function(app) {
     
     app.route('/tampil')
         .get(jsonku.tampilDataMahasiswa);
+
+    app.route('/tampil/:id')
+        .get(jsonku.tampilMahasiswabyID);
 }
