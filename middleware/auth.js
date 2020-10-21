8e03b3f55b47eb14cd4cdcabf130bede26d394c0
rian <rianwahyu26@gmail.com>
2020-10-21 17:18:45 +0700

    login user

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,6 +5,7 @@ var response = require('../res');
 var jwt = require('jsonwebtoken');
 var config = require('../config/secret');
 var ip = require('ip');
+const secret = require('../config/secret');
 
 exports.registrasi = function(req, res){
     var post = {
@@ -15,7 +16,7 @@ exports.registrasi = function(req, res){
         tanggal_daftar: new Date()
     }
     
-    var query = "SELECT email FROM ?? WHERE ?? ";
+    var query = "SELECT email FROM ?? WHERE ??=?";
     var table = ["user", "email", post.email];
 
     query = mysql.format(query, table);
@@ -36,8 +37,63 @@ exports.registrasi = function(req, res){
                     }
                 });
             }else{
-                response.ok("Email sudah terdaftar !");
+                response.ok("Email sudah terdaftar !", res);
             }
         }
     })
+}
+
+//controller untuk login
+
+exports.login = function(req, res){
+    var post = {
+        password: req.body.password,
+        email: req.body.email
+    }
+
+    //cek user yg ada
+    var query = "SELECT * FROM ?? WHERE ??=? AND ??=?";
+    var table = ["user", "password", md5(post.password), "email", post.email];
+    
+    query = mysql.format(query, table);
+
+    connection.query(query, function(error, rows){
+        if(error){
+            console.log(error);
+        }else{
+            //jika ada
+            if(rows.length == 1){
+                //membuat generated token
+                var token = jwt.sign({rows}, config.secret, {
+                    expiresIn: 1440
+                });
+
+                id_user = rows[0].id;
+                var data = {
+                    id_user: id_user, 
+                    access_token: token, 
+                    ip_address: ip.address()
+                }
+
+                var query = "INSERT INTO ?? SET ?";
+                var table = ["akses_token"];
+                
+                query = mysql.format(query, table);
+                connection.query(query, data, function(error, rows){
+                    if (error){
+                        console.log(error);
+                    }else{
+                         res.json({
+                             success: true, 
+                             message: 'Token JWT Generated',
+                             token: token, 
+                             curUser: data.id_user
+                         });
+                    }
+                })
+            }else{
+                 res.json({"Error": true, "Message":"Email atau password salah"});
+            }
+        }
+    });
 }
--- middleware/index.js
+++ middleware/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var auth = require('./auth');
 var router = express.Router();
 
-
+   
 //mendaftarkan menu registrasi
 router.post('/api/v1/register', auth.registrasi);
+
+router.post('/api/v1/login', auth.login);
 
 module.exports = router;
