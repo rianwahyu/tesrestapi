cd7ed613885b15418bafabb99bf0d67dcad45e51
rian <rianwahyu26@gmail.com>
2020-10-26 14:44:04 +0700

    menambahkan role pada login

--- middleware/index.js
+++ middleware/index.js
@@ -2,10 +2,16 @@ var express = require('express');
 var auth = require('./auth');
 var router = express.Router();
 
+var verifikasi = require('./verifikasi');
+
    
 //mendaftarkan menu registrasi
 router.post('/api/v1/register', auth.registrasi);
 
 router.post('/api/v1/login', auth.login);
+
+//alamat yg perlu otorisasi
+
+router.get('/api/v1/rahasia', verifikasi(), auth.halamanrahasia);
 
 module.exports = router;
